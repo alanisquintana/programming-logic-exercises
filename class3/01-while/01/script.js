e972a9533bc8e67aca56e7729e498acb11cfa110
Alanis Quintana <alanisquintana.dev@gmail.com>
2024-06-16 14:29:34 -0300

    Class3

---
+++ class3/01-while/01/script.js
@@ -0,0 +1,10 @@
+/* 
+You are developing a timer and need to create a countdown from an initial number. Write a code that uses a loop to print the numbers from 10 to 1.
+*/
+
+let counter = 10
+
+while (counter >= 1) {
+    console.log(counter)
+    counter--
+}
